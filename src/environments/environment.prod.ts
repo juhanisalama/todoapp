8a5644129d8e013089f895ac7740fddb18af891c
Juhani <juhani.salama@magister.fi>
2021-12-06 17:11:15 +0200

    Finished the app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: 'angulartodoapp-ed1e6',
+    appId: '1:104203878784:web:038067a6a0d6e9f6910165',
+    storageBucket: 'angulartodoapp-ed1e6.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBJpOh68YZRFS84CCYH2npGdZZDL8l1uko',
+    authDomain: 'angulartodoapp-ed1e6.firebaseapp.com',
+    messagingSenderId: '104203878784',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'angulartodoapp-ed1e6',
+    appId: '1:104203878784:web:038067a6a0d6e9f6910165',
+    storageBucket: 'angulartodoapp-ed1e6.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBJpOh68YZRFS84CCYH2npGdZZDL8l1uko',
+    authDomain: 'angulartodoapp-ed1e6.firebaseapp.com',
+    messagingSenderId: '104203878784',
+  },
 };
 
 /*
